5a922224d7907ecfd816904a770c51c775238dc6
Yashwant Dongare <madhavdongre19636@gmail.com>
2026-07-09 16:13:50 +0530

    Add files via upload

---
+++ Table.c
@@ -0,0 +1,14 @@
+Name- Dongare Yashwant madhav
+PRN No- 2503033111372029
+#include <stdio.h>
+int main(){
+    int i,table,num;
+    printf("Enter A Number: ");
+    scanf("%d",&num);
+    
+    for(i=1;i<=10;i++){
+        table=i*num;
+        printf("%d\n",table);
+    }
+    return 0;
+}
