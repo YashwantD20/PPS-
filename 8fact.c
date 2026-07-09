a21312f5994ad1e7473f4214680f94d48d8879b0
Yashwant Dongare <madhavdongre19636@gmail.com>
2026-07-09 16:35:01 +0530

    Add files via upload

---
+++ 8fact.c
@@ -0,0 +1,23 @@
+/*Name- Dongare Yashwant Madhav 
+PRN No- 2503033111372029
+#include <stdio.h>
+int factorial(int n);
+
+int main(){
+    int num,ans;
+    printf("Enter a number: ");
+    scanf("%d",&num);
+
+    ans=factorial(num);
+    printf("Factorial is %d\n",ans);
+    return 0;
+}
+
+int factorial(int n){
+    if(n==0||n==1){
+        return 1;
+    }
+    else{
+        return n*factorial(n-1);
+    }
+}
